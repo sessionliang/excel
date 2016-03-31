28bb750a162cc7a1caa481c23a156a7fc3b8b4e7
sessionliang <liangjian@siteserver.cn>
2016-03-31 14:31:32 +0800

    init

---
+++ Excel/Excel/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnImport = new System.Windows.Forms.Button();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbFile = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbKeywords = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSavePath = new System.Windows.Forms.Button();
+            this.tbSavePath = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnComfirm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnImport
+            // 
+            this.btnImport.Location = new System.Drawing.Point(383, 64);
+            this.btnImport.Name = "btnImport";
+            this.btnImport.Size = new System.Drawing.Size(43, 27);
+            this.btnImport.TabIndex = 0;
+            this.btnImport.Text = "...";
+            this.btnImport.UseVisualStyleBackColor = true;
+            this.btnImport.Click += new System.EventHandler(this.btnImport_Click);
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(115, 22);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(262, 25);
+            this.tbName.TabIndex = 1;
+            this.tbName.Text = "vivo";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(57, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "产品：";
+            // 
+            // tbFile
+            // 
+            this.tbFile.Location = new System.Drawing.Point(115, 66);
+            this.tbFile.Name = "tbFile";
+            this.tbFile.Size = new System.Drawing.Size(262, 25);
+            this.tbFile.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "excel文件：";
+            // 
+            // tbKeywords
+            // 
+            this.tbKeywords.Location = new System.Drawing.Point(115, 146);
+            this.tbKeywords.Multiline = true;
+            this.tbKeywords.Name = "tbKeywords";
+            this.tbKeywords.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbKeywords.Size = new System.Drawing.Size(262, 154);
+            this.tbKeywords.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "关键字：";
+            // 
+            // btnSavePath
+            // 
+            this.btnSavePath.Location = new System.Drawing.Point(383, 103);
+            this.btnSavePath.Name = "btnSavePath";
+            this.btnSavePath.Size = new System.Drawing.Size(43, 26);
+            this.btnSavePath.TabIndex = 0;
+            this.btnSavePath.Text = "...";
+            this.btnSavePath.UseVisualStyleBackColor = true;
+            this.btnSavePath.Click += new System.EventHandler(this.btnSavePath_Click);
+            // 
+            // tbSavePath
+            // 
+            this.tbSavePath.Location = new System.Drawing.Point(115, 106);
+            this.tbSavePath.Name = "tbSavePath";
+            this.tbSavePath.Size = new System.Drawing.Size(262, 25);
+            this.tbSavePath.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 116);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 15);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "保存路径：";
+            // 
+            // btnComfirm
+            // 
+            this.btnComfirm.Location = new System.Drawing.Point(115, 306);
+            this.btnComfirm.Name = "btnComfirm";
+            this.btnComfirm.Size = new System.Drawing.Size(262, 45);
+            this.btnComfirm.TabIndex = 5;
+            this.btnComfirm.Text = "确  定";
+            this.btnComfirm.UseVisualStyleBackColor = true;
+            this.btnComfirm.Click += new System.EventHandler(this.btnComfirm_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 372);
+            this.Controls.Add(this.btnComfirm);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.tbKeywords);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbSavePath);
+            this.Controls.Add(this.tbFile);
+            this.Controls.Add(this.btnSavePath);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.btnImport);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnImport;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbFile;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbKeywords;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSavePath;
+        private System.Windows.Forms.TextBox tbSavePath;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnComfirm;
+    }
+}
+
---
+++ Excel/Excel/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //excel文件路径
+        private string excelFilePath;
+        //保存文件路径
+        private string saveFilePath;
+        //关键字集合，关键字用来做模糊查询分组
+        private List<string> keywordList;
+
+        //源表字段
+        private const string SHCK = "收货仓库";
+        private const string YS = "颜色";
+        private const string SPMC = "商品名称";
+        private const string CH = "串号";
+        private const string MEID = "meid";
+
+        //目标表字段
+        private const string XH = "序号";
+        private const string JX = "机型";
+        private const string CM = "串码";
+        private const string RKRQ = "入库日期";
+        private const string BZ = "备注";
+
+
+        /// <summary>
+        /// 选择excel文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnImport_Click(object sender, EventArgs e)
+        {
+            tbFile.Text = excelFilePath = OpenDialogAndSelectFile();
+        }
+
+        /// <summary>
+        /// 选择保存路径
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSavePath_Click(object sender, EventArgs e)
+        {
+            tbSavePath.Text = saveFilePath = OpenDialogAndSaveFile();
+        }
+
+        /// <summary>
+        /// 确定按钮点击
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnComfirm_Click(object sender, EventArgs e)
+        {
+            /* * * * * * * * * * * * * * * * *
+             * 序号  机型  串码  入库日期  备注 *
+             * * * * * * * * * * * * * *  * * */
+            InitKeywordList();
+            string dbCmdStr = string.Empty;
+            string selectStr = "SELECT * FROM {0} ";
+            string whereStr = " WHERE 1=1 ";
+            string groupStr = string.Format(" GROUP BY {0} ", SHCK);
+            foreach (string keyword in keywordList)
+            {
+                whereStr += string.Format(" AND {0} NOT LIKE '%{1}%' ", SHCK, keyword);
+                whereStr += string.Format(" AND {0} NOT LIKE '%{1}' ", SHCK, keyword);
+                whereStr += string.Format(" AND {0} NOT LIKE '{1}%' ", SHCK, keyword);
+            }
+            dbCmdStr = String.Join(" ", selectStr, whereStr, groupStr);//排除掉通过关键字帅选的数据，然后分组
+
+            whereStr = " WHERE 1=1 ";
+            foreach (string keyword in keywordList)
+            {
+                whereStr += string.Format(" AND {0} LIKE '%{1}%' ", SHCK, keyword);
+                whereStr += string.Format(" AND {0} LIKE '%{1}' ", SHCK, keyword);
+                whereStr += string.Format(" AND {0} LIKE '{1}%' ", SHCK, keyword);
+                dbCmdStr += " UNION " + String.Join(" ", selectStr, whereStr, groupStr);
+            }
+            DataTable dt = GetContentsByExcelFile(dbCmdStr);
+        }
+
+        /// <summary>
+        /// 弹出对话框，选择文件
+        /// </summary>
+        public string OpenDialogAndSelectFile()
+        {
+            string path = "";
+            using (OpenFileDialog dialog = new OpenFileDialog())
+            {
+                dialog.Filter = "文本文件(*.xls)|*.xls";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = dialog.FileName;
+                }
+            }
+            return path;
+        }
+
+        /// <summary>
+        /// 弹出对话框，选择文件保存路劲
+        /// </summary>
+        public string OpenDialogAndSaveFile()
+        {
+            string path = "";
+            using (SaveFileDialog dialog = new SaveFileDialog())
+            {
+                dialog.Filter = "文本文件(*.xls)|*.xls";
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    path = dialog.FileName;
+                }
+            }
+            return path;
+        }
+
+        /// <summary>
+        /// 转换excel中的sheet为datatable
+        /// </summary>
+        /// <param name="oleDbCmdStr">选择sql语句，{0}代表表名</param>
+        /// <returns></returns>
+        public DataTable GetContentsByExcelFile(string oleDbCmdStr)
+        {
+            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + excelFilePath + ";Extended Properties=Excel 8.0;";
+            OleDbConnection oleDbConn = new OleDbConnection(strConn);
+            OleDbCommand oleDbCmd;
+            OleDbDataAdapter oleDbAdp;
+            DataTable oleDt = new DataTable();
+            DataTable dtTableName = new DataTable();
+
+            oleDbConn.Open();
+            dtTableName = oleDbConn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+            string[] tableNames = new string[dtTableName.Rows.Count];
+            for (int j = 0; j < dtTableName.Rows.Count; j++)
+            {
+                tableNames[j] = dtTableName.Rows[j]["TABLE_NAME"].ToString();
+            }
+
+            foreach (string tableName in tableNames)
+            {
+                if (!tableName.EndsWith("$"))
+                    continue;
+
+                try
+                {
+                    oleDbCmd = new OleDbCommand(string.Format(oleDbCmdStr, tableName), oleDbConn);
+                    oleDbAdp = new OleDbDataAdapter(oleDbCmd);
+                    oleDbAdp.Fill(oleDt);
+                    oleDbConn.Close();
+
+                    //if (oleDt.Rows.Count > 0)
+                    //{
+
+                    //    ArrayList attributeNames = new ArrayList();
+                    //    for (int i = 0; i < oleDt.Columns.Count; i++)
+                    //    {
+                    //        string columnName = oleDt.Columns[i].ColumnName;
+                    //        if (!attributeNames.Contains(columnName))
+                    //            attributeNames.Add(columnName);
+                    //    }
+
+                    //    foreach (DataRow row in oleDt.Rows)
+                    //    {
+                    //        for (int i = 0; i < oleDt.Columns.Count; i++)
+                    //        {
+                    //            string attributeName = attributeNames[i] as string;
+                    //            if (!string.IsNullOrEmpty(attributeName))
+                    //            {
+                    //                string value = row[i].ToString();
+
+                    //            }
+                    //        }
+                    //    }
+                    //}
+                }
+                catch { }
+            }
+
+            return oleDt;
+        }
+
+        /// <summary>
+        /// 初始化关键字集合
+        /// </summary>
+        public void InitKeywordList()
+        {
+            var keywordArr = tbKeywords.Text.Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            keywordList = new List<string>(keywordArr);
+        }
+    }
+}
